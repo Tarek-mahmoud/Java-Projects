b4b323abee2ac5a2c54a25025c60b1e28eaea10d
Tarek Mahmoud <tarekashm@gmail.com>
2024-10-16 18:40:50 +0300

    Add files via upload

---
+++ Console/ATM/ATM.java
@@ -0,0 +1,12 @@
+package Section12.ATM;
+
+import java.io.IOException;
+
+public class ATM extends OptionMenu {
+    public static void main(String[] args) throws IOException {
+
+        OptionMenu optionMenu = new ATM();
+
+        optionMenu.getLogin();
+    }
+}
